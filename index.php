4f9a1e352e305203fe16151359ba5490d59013d2
admin <admin@gitsec.org>
2024-12-15 02:38:49 +0100

    Added user session

--- index.php
+++ index.php
@@ -9,8 +9,17 @@ $db_password = "";
 $dbconn = pg_connect("host=$db_host dbname=$db_name user=$db_user password=$db_password")
     or die('Could not connect: ' . pg_last_error());
 
-// Previous versions can be restored
-// CTF{It doesn't even matter how hard you try}
+// Authenticate user
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$result = pg_prepare($dbconn, "prepared_query", 'SELECT name FROM users WHERE name = $1 AND password = $2');
+$result = pg_execute($dbconn, "prepared_query", array($username, $password));
+
+if ($result) {
+  session_start();
+  $_SESSION["username"]=$pg_fetch_row($result)[0];
+}
 
 // Close database connection
 pg_close($dbconn);
@@ -21,6 +30,8 @@ pg_close($dbconn);
   <body>
     <h1>Login to the admin portal</h1>
     <form method="POST">
+      <!-- HTML comments are sent to users -->  
+      <!-- CTF{It starts with one, one thing, I don't know why} -->
       <div>User name</div>
       <input name="username" type="text"/>
       <div>Password</div>
